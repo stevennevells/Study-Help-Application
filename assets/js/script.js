e2e3041b9c31542fd78d5ec0de14e166161f60f3
Alexia Valenzuela <103925327+alexiaValen@users.noreply.github.com>
2022-06-06 20:57:31 -0700

    added search  literaure function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,34 @@ var historyButton = document.querySelector('.history-button');
 
 // Event listeners
 mathButton.addEventListener('click', searchMath);
-// literatureButton.addEventListener('click', searchLiterature);
+literatureButton.addEventListener('click', searchLiterature);
 // scienceButton.addEventListener('click', searchScience);
 // historyButton.addEventListener('click', searchHistory);
 
 function searchMath () {
     var apiUrl = "https://openlibrary.org/subjects/mathemathics.json";
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    //put function for populating screen here
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to API');
+            });
+}
+
+
+
+function searchLiterature () {
+    var apiUrl = "https://openlibrary.org/subjects/literature.json";
 
     fetch(apiUrl)
         .then(function (response) {

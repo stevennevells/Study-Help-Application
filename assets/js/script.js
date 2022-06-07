a9e005512877495ea4ceb1b757326dfcb1f235f5
Villette Comfort <villette@live.com>
2022-06-06 20:50:35 -0700

    Added variables for query selectors and get function for math button

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var mathButton= document.querySelector('.math-button');
+var literatureButton= document.querySelector('.literature-button');
+var scienceButton= document.querySelector('.science-button');
+var historyButton = document.querySelector('.history-button');
+
+// Event listeners
+mathButton.addEventListener('click', searchMath);
+// literatureButton.addEventListener('click', searchLiterature);
+// scienceButton.addEventListener('click', searchScience);
+// historyButton.addEventListener('click', searchHistory);
+
+function searchMath () {
+    var apiUrl = "https://openlibrary.org/subjects/mathemathics.json";
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    //put function for populating screen here
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to API');
+            });
+}
